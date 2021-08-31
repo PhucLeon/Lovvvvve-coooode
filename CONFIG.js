b6784cceea9aee31564b924d760336a510c610f1
Phuc Leon <hachimanhikki191@gmail.com>
2021-08-31 23:17:12 +0700

    Please play the audio

---
+++ CONFIG.js
@@ -0,0 +1,12 @@
+const CONFIGDATA = {
+  titleHeader: "Chúc Mừng bấy bì đã vào FU",
+  descriptionHeader:
+    "Giờ là lúc tặng quà rồi ^^<br>Lúc vui, em có thể luyên thuyên mọi thứ với món quà này<br>Lúc cô đơn nhất, món quà sẽ lắng nghe tâm sự của em<br>Món quà này sẽ không bỏ rơi em nếu em không bỏ rơi nó<br>VÀ<br>Món quà này chưa từng được ai sử dụng ^^<br><br>Đúng rồi ^^, là anh á. Em đồng ý nhận hông ^^. Nếu thoát ra xem như đồng ý nha kkk",
+  buttonYes: "Đồng Ý",
+  buttonNo: "Không! Còn lâu nhé.",
+  
+  //WHEN USER CLICK ON ĐỒNG Ý BUTTON
+  titleModar: "Cảm ơn em vì đồng ý",
+  descriptionModar:
+    'Món quà này hứa che chở và bảo vệ em.<br> Và, sẽ không bỏ rơi em ^^ :)) </br> Lý do á ^^, nhắn anh đi vì giờ anh đỏ mặt hết ời :))',
+};
