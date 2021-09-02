6bebb7d5c4c196abdee7af0e3465b6e1a36b069b
Phuc Leon <hachimanhikki191@gmail.com>
2021-09-02 12:46:48 +0700

    final change

--- CONFIG.js
+++ CONFIG.js
@@ -1,5 +1,5 @@
 const CONFIGDATA = {
-  titleHeader: "Chúc Mừng bấy bì đã vào FU",
+  titleHeader: "Món quà siêu cute ^^",
   descriptionHeader:
     "Giờ là lúc tặng quà rồi ^^<br>Lúc vui, em có thể luyên thuyên mọi thứ với món quà này<br>Lúc cô đơn nhất, món quà sẽ lắng nghe tâm sự của em<br>Món quà này sẽ không bỏ rơi em nếu em không bỏ rơi nó<br>VÀ<br>Món quà này chưa từng được ai sử dụng ^^<br><br>Đúng rồi ^^, là anh á. Em đồng ý nhận hông ^^. Nếu thoát ra xem như đồng ý nha kkk",
   buttonYes: "Đồng Ý",
